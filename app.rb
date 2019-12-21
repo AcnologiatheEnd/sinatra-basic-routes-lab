7395e7dec33c391034832f3d538edfa9fa3bc34b
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-21 15:34:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get '/name' do
     'My name is slim shady'
   end
+  
+   get '/hometown' do
+    'My hometown is beattlejuice'
+  end
+  
+   get '/favorite-song' do
+    'one more time'
+  end
 end
